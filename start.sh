1dd2b141d6fc580240a55fd88145dddf3fbc0cfe
anonymousGiga <linchhit@163.com>
2021-10-23 16:39:04 +0800

    add start script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OASIS_CORE=~/.oasis/core/v21.3.1/bin
+RUNTIME_DIR=/tmp/eth-runtime-test
+RUNTIME_BINARY=target/debug/eth-runtime
+
+# Make sure we start with a clean data directory.
+rm -rf ${RUNTIME_DIR}
+mkdir -p ${RUNTIME_DIR}
+
+${OASIS_CORE}/oasis-net-runner \
+	--fixture.default.node.binary ${OASIS_CORE}/oasis-node \
+	--fixture.default.runtime.binary ${RUNTIME_BINARY} \
+	--fixture.default.runtime.loader ${OASIS_CORE}/oasis-core-runtime-loader \
+	--fixture.default.keymanager.binary '' \
+	--basedir ${RUNTIME_DIR} \
+	--basedir.no_temp_dir
